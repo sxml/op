8a6f60e9cbcfe7138a1ffbba470746f4355a9250
sxml <59968675@qq.com>
2020-11-29 12:12:52 +0800

    Update diy-part2.sh

--- router/s905x3_phicomm-n1/diy-part2.sh
+++ router/s905x3_phicomm-n1/diy-part2.sh
@@ -25,8 +25,10 @@ svn co https://github.com/ophub/op/trunk/router/s905x3_phicomm-n1/install-progra
 
 # passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray package/Xray-core
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray package/Xray
 
+./scripts/feeds update -a
+./scripts/feeds install -a
 # miniupnpd
 # svn co https://github.com/openwrt/packages/trunk/net/miniupnpd package/miniupnpd
 
